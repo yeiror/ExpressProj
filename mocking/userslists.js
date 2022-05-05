cd07cbb002ba3c971d7b4960b30b5b5a8eedc414
Raul Ruiz <rf.ruiz10@gmail.com>
2022-05-05 17:56:49 -0500

    added modules

--- mocking/userslists.js
+++ mocking/userslists.js
@@ -3,11 +3,6 @@ const faker = require("faker");
 const status = ['ACTIVE','VERIFIED','BANNED','DELETED'];
 let userList = []
 
-
-//let id= faker.datatype.uuid();
-//let fullName= faker.name.findName();
-//let urlImage= faker.image.avatar();
-//let dateOfRegister= faker.date.future();
 let randomUsers = (amount) => {
 
   for ( i = 0; i< amount;  i++){
@@ -15,9 +10,9 @@ let randomUsers = (amount) => {
     fakeFullName = faker.name.findName(),
     fakeUrlImage = faker.image.avatar(),
     fakeDateOfRegister = faker.date.future(),
-    status = Math.round(Math.random() * 4)
+    fakeStatus = status[Math.floor(Math.random() * 4)]
     //profile= 2
-    userList.push(fakeId, fakeFullName, fakeUrlImage, fakeDateOfRegister, status)
+    userList.push(fakeId, fakeFullName, fakeUrlImage, fakeDateOfRegister, fakeStatus)
   }
   return userList;
 }
